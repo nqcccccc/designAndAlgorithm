2dfefbe3b431022dc292d2b0d09ecbba416665e7
nqcccccc <cuongbo1324@gmail.com>
2020-11-25 22:34:48 +0700

    25/11-10h30

--- bestone/nQueens-Psudocode.py
+++ bestone/nQueens-Psudocode.py
@@ -2,64 +2,78 @@ import sys
 import random
 import time
 import pylab
+# Function isSafe use to check to Queens is safe OR not by checking the row on left side
+#,upper AND lower diagonal on th left 
+#Input: board[],current row AND collum 
+#Outpt: True for safe AND False for not safe
+             ENDFOR
+FUNCTION isSafe(board, row, col): 
+ENDFUNCTION
 
-# Function isSafe use to check to Queens is safe or not by checking the row on left side
-#,upper and lower diagonal on th left 
-#Input: board[],current row and collum 
-#Outpt: True for safe and False for not safe
-def isSafe(board, row, col): 
 	for i in range(col): 
-		if board[row][i] == 1: 
+ ENDFOR
+		if board[row][i] = 1: 
+  ENDIF
 			return False
 	for i, j in zip(range(row, -1, -1), 
+ ENDFOR
 					range(col, -1, -1)): 
-		if board[i][j] == 1: 
+		if board[i][j] = 1: 
+  ENDIF
 			return False
 	for i, j in zip(range(row, N, 1), 
+ ENDFOR
 					range(col, -1, -1)): 
-		if board[i][j] == 1: 
+		if board[i][j] = 1: 
+  ENDIF
 			return False
 	return True
-
 #nQueens(board[],col)
 #Function use to the problem of placing N chess queens on an N×N chessboard so
 #that no two queens attack each other.
 #Input: board[],start collum
 #Output: Matrix for above N queen solution
-def nQueens(board, col): 
+                ENDFOR
+FUNCTION nQueens(board, col): 
+ENDFUNCTION
+
 	if col >= N: 
+ ENDIF
 		return True
 	for i in range(N): 
+ ENDFOR
 		if isSafe(board, i, col): 
-			board[i][col] = 1
-			if nQueens(board, col + 1) == True: 
+  ENDIF
+			board[i][col] <- 1
+			if nQueens(board, col + 1) = True: 
+   ENDIF
 				return True
-			board[i][col] = 0
+			board[i][col] <- 0
 	return False
-
 N=4
-board = [[0]*N for _ in range(N)]
+board <- [[0]*N for _ in range(N)]
+               ENDFOR
 nQueens(board, 0)
 for i in board:
-    print(i)
+    OUTPUT i
 # [0, 0, 1, 0]
 # [1, 0, 0, 0]
 # [0, 0, 0, 1]
 # [0, 1, 0, 0]
-
-# x = []
-# y = []
+# x <- []
+# y <- []
 # for i in range(4,20):
-# 	N = i
+  ENDFOR
+# 	N <- i
 # 	x.append(i)
-# 	board = [[0]*i for _ in range(i)]
+# 	board <- [[0]*i for _ in range(i)]
+                  ENDFOR
 # 	start= time.time()
 # 	nQueens(board, 0)
 # 	stop= time.time()
 # 	y.append(stop-start)
-
 # pylab.plot(x,y,'ro-')
 # pylab.title("N Queens Algorithim")
 # pylab.xlabel('Input size')
 # pylab.ylabel('Execution time (Seconds)')
-# pylab.show()
+# pylab.show(
